bd6b51f087d97a80b2469f210eaa30e1af92911a
labrador936 <40704323+labrador936@users.noreply.github.com>
2018-10-27 21:10:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ const queue = new Map();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`By Luca Changretta |;help`,"http://twitch.tv/S-F")
+client.user.setGame(`By sweet home |;help`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('+[-----------------------------------------------------------------]+')
